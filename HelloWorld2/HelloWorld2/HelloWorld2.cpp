4b5222b78c5519caef22f0beadacb5c79861c823
Ahmet Karamanoğlu <ahmetkaramanoglu678@gmail.com>
2020-11-05 01:46:04 +0300

    Pointer konusu işlendi

--- HelloWorld2/HelloWorld2/HelloWorld2.cpp
+++ HelloWorld2/HelloWorld2/HelloWorld2.cpp
@@ -1,22 +1,30 @@
-﻿// HelloWorld2.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
+
 int main()
 {
-    cout << "Hello World!\n";
-    cout << "Merhaba Dunya";
-    cout << "Selamlar...";
+	int dizi[1];
+	dizi[0] = 5;
+	int* ptr;
+	ptr = dizi;
+
+	cout << "Dizinin adresi..: " << ptr << endl;
+	cout << "Dizinin degeri..: " << *ptr << endl;
+
+	cout << "*******************************" << endl << endl;
+
+	int dizi2[3] = { 5,10,15 };
+	int* myPtr;
+	myPtr = dizi2;
+	*myPtr = 6;
+	cout << "Dizinin degistirilen ilk elemani..: " << dizi2[0] << endl << endl;
+
+	for (int i = 0; i < 3; i++) {
+		cout << "Dizinin " << i + 1 << ". degeri..: " << *(myPtr + i) << endl;
+	}
+		cout << endl << endl;
+
+		for (int i = 0; i <3; i++) {
+			cout << "Dizinin " << i + 1 << ". degerinin adresi(4 bayt artiyor.)..: " << (myPtr + i) << endl;
+		}
 }
-
-// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
-// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
-
-// Kullanmaya Başlama İpuçları: 
-//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
-//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
-//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
-//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
-//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
-//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
